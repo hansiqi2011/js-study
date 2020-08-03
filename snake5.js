22e3547f07f80e83dc5da7418121b52a5d252e86
Siqi Han <hansiqi2011@outlook.com>
2020-08-03 10:21:50 +0800

    set posation

--- snake5.js
+++ snake5.js
@@ -1,6 +1,7 @@
 let appleColors=["blue","yellow","green"];
-var snake=[{x:8,y:10}, {x:8, y:11}, {x:8, y:12}];
-var appleColor;
+var snake=[{x:8,y:10,direction:"left"}, {x:9,y:10,direction:"left"}, {x:10,y:10,direction:"left"}];
+
+var appleColor=appleColors[random()*appleColors.length];
 var applePosation=[100,100];
 function setup() {
     createCanvas(400, 400);
@@ -13,21 +14,26 @@ function drawWalls() {
 }
 
 function drawSnakePiece(x, y, color="blue", size=8) {
+    fill(color);
     rect(x*size-size/2, y*size-size/2, size, size);
+    noFill();
 }
 
 function drawSnake(color="blue") {
     strokeWeight(0);
     fill(color);
     snake.forEach(piece => {
-        drawSnakePiece(piece.x, piece.y);
+        drawSnakePiece(piece.x,piece.y,color);
     });
     noFill();
 }
 
 function updateSnake() {
     snake.forEach(piece=> {
-        piece.y = piece.y+0.1;
+        if (piece.direction === "left"){
+            piece.x-1;
+            piece.posation=[piece.x,piece.y];
+        }
     });
 }
 
