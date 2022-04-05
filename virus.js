3ed9a1706b48e7e1464198a1000c9f5f0457129c
Siqi Han <hansiqi2011@outlook.com>
2022-04-05 18:48:43 +0800

    adding: cure time & spread speed input

--- virus.js
+++ virus.js
@@ -1,5 +1,6 @@
-const INIT_PERSON_NUMBER = 100;
-
+const INIT_PERSON_NUMBER = 50;
+let curetime = 10;
+let spreadSpeed = 0.01;
 function setup() {
     createCanvas(600, 600);
     background("#969696");
@@ -17,9 +18,7 @@ for (let index = 0; index < INIT_PERSON_NUMBER; index++) {
         isPositive: false,
     });
 }
-for (let index = 0; index < 1; index++) {
-    samples[index].isPositive = true;
-}
+samples[0].isPositive = true;
 function updatePosition(sample) {
     if (random() < 0.01) {
         sample.xs = randInt(-2, 2);
@@ -31,23 +30,54 @@ function updatePosition(sample) {
     if (sample.y <= 0 || sample.y >= 600) sample.ys *= -1;
 }
 function updateState(sample1, sample2) {
-    if (Math.random() <= 0.005) {
-        if (sample1.isPositive) sample2.isPositive = true;
-        else if (sample2.isPositive) sample1.isPositive = true;
+    if (Math.random() <= spreadSpeed) {
+        if (sample1.isPositive && !sample2.isPositive) {
+            sample2.isPositive = true;
+            clearTimeout("smp1");
+            setTimeout(
+                () => (sample2.isPositive = false),
+                curetime * 1000,
+                "smp2"
+            );
+        } else if (sample2.isPositive && !sample1.isPositive) {
+            sample1.isPositive = true;
+            clearTimeout("smp2");
+            setTimeout(
+                () => (sample1.isPositive = false),
+                curetime * 1000,
+                "smp1"
+            );
+        }
     }
 }
 function draw() {
     fill(150);
     rect(0, 0, 600, 600);
-    samples.forEach((element) => {
-        updatePosition(element);
-        fill(element.isPositive ? "#f00" : "#0f0");
-        ellipse(element.x, element.y, 10, 10);
-    });
-    samples.forEach((elm1) =>
+    samples.forEach((elm1) => {
+        updatePosition(elm1);
+        fill(elm1.isPositive ? "#f00" : "#0f0");
+        ellipse(elm1.x, elm1.y, 10, 10);
         samples.forEach((elm2) => {
             if (dist(elm1.x, elm1.y, elm2.x, elm2.y) <= 30)
                 updateState(elm1, elm2);
-        })
-    );
+        });
+    });
+}
+function reset() {
+    samples = [];
+    for (let index = 0; index < INIT_PERSON_NUMBER; index++) {
+        samples.push({
+            x: randInt(0, 600),
+            y: randInt(0, 600),
+            xs: randInt(-2, 2),
+            ys: randInt(-2, 2),
+            isPositive: false,
+        });
+    }
+    samples[0].isPositive = true;
+}
+function change() {
+    spreadSpeed = document.getElementById("spreadSpeed").value;
+    spreadSpeedOutput = document.getElementById("spreadSpeedOutput");
+    spreadSpeedOutput.innerHTML = str(spreadSpeed * 100) + "%";
 }
