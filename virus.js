bbca24f8592f27ed0bc384f7d32b7d73ca690fda
Siqi Han <hansiqi2011@outlook.com>
2022-04-11 20:39:21 +0800

    add antibody

--- virus.js
+++ virus.js
@@ -1,9 +1,10 @@
 const INIT_PERSON_NUMBER = 50;
-let curetime = 100;
+const WORLD_SIZE = 400;
+let curetime = 1000;
 let spreadSpeed = 0.01;
 let infetedPeopleNumber = 1;
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(WORLD_SIZE, WORLD_SIZE);
     background("#000000");
     noStroke();
 }
@@ -11,16 +12,17 @@ const randInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 let samples = [];
 for (let index = 0; index < INIT_PERSON_NUMBER; index++) {
     samples.push({
-        x: randInt(0, 600),
-        y: randInt(0, 600),
+        x: randInt(0, WORLD_SIZE),
+        y: randInt(0, WORLD_SIZE),
         xs: randInt(-2, 2),
         ys: randInt(-2, 2),
         isPositive: false,
         healthLevel: curetime * 1000,
-        antibudyLevel: 0,
+        antibodyLevel: 0,
     });
 }
 samples[0].isPositive = true;
+samples[0].antibodyLevel = 1000;
 samples[0].healthLevel = 0;
 function updatePosition(sample) {
     if (random() < 0.01) {
@@ -29,28 +31,41 @@ function updatePosition(sample) {
     }
     sample.x += sample.xs;
     sample.y += sample.ys;
-    if (sample.x <= 0 || sample.x >= 600) sample.xs *= -1;
-    if (sample.y <= 0 || sample.y >= 600) sample.ys *= -1;
+    if (sample.x <= 0 || sample.x >= WORLD_SIZE) sample.xs *= -1;
+    if (sample.y <= 0 || sample.y >= WORLD_SIZE) sample.ys *= -1;
 }
-const updateState = (sample1, sample2) => {
+function updateState(sample1, sample2) {
     if (Math.random() <= spreadSpeed) {
-        sample1.isPositive = true;
-        sample1.healthLevel = 0;
-        sample2.isPositive = true;
-        sample2.healthLevel = 0;
+        if (sample1.antibodyLevel == 0) {
+            sample1.isPositive = true;
+            sample1.antibodyLevel = 1000;
+            sample1.healthLevel = 0;
+        }
+        if (sample2.antibodyLevel == 0) {
+            sample2.isPositive = true;
+            sample2.antibodyLevel = 1000;
+            sample2.healthLevel = 0;
+        }
     }
-};
-
+}
 function draw() {
     infetedPeopleNumber = 0;
     fill(0);
-    rect(0, 0, 600, 600);
+    rect(0, 0, WORLD_SIZE, WORLD_SIZE);
     samples.forEach((elm1) => {
         if (elm1.isPositive) infetedPeopleNumber++;
-        if (elm1.healthLevel < 100) elm1.healthLevel++;
-        else elm1.isPositive = false;
+        if (elm1.healthLevel < 100) {
+            elm1.healthLevel++;
+            if (elm1.healthLevel == 0) elm1.antibodyLevel = 1;
+        } else elm1.isPositive = false;
         updatePosition(elm1);
-        fill(elm1.isPositive ? "#f00" : "#0f0");
+        if (elm1.isPositive) {
+            fill("#f00");
+        } else if (elm1.antibodyLevel > 0) {
+            fill("orange");
+        } else {
+            fill("#0f0");
+        }
         ellipse(elm1.x, elm1.y, 10, 10);
         samples.forEach((elm2) => {
             if (
@@ -61,18 +76,22 @@ function draw() {
         });
     });
     document.getElementById("infectionOutput").innerHTML =
-        "infected people number: " + str(infetedPeopleNumber) + "/50";
+        "infected people number: " +
+        str(infetedPeopleNumber) +
+        "/" +
+        str(INIT_PERSON_NUMBER);
 }
 function reset() {
     samples = [];
     for (let index = 0; index < INIT_PERSON_NUMBER; index++) {
         samples.push({
-            x: randInt(0, 600),
-            y: randInt(0, 600),
+            x: randInt(0, WORLD_SIZE),
+            y: randInt(0, WORLD_SIZE),
             xs: randInt(-2, 2),
             ys: randInt(-2, 2),
             isPositive: false,
             healthLevel: curetime * 1000,
+            antibodyLevel: 0,
         });
     }
     samples[0].isPositive = true;
@@ -84,7 +103,7 @@ function change() {
     spreadSpeedOutput.innerHTML = str(spreadSpeed * 100) + "%";
 }
 function addPositive() {
-    let unluckyman = samples[randInt(1, INIT_PERSON_NUMBER)];
-    unluckyman.isPositive = true;
-    unluckyman.healthLevel = 0;
+    let positive = samples[randInt(1, INIT_PERSON_NUMBER)];
+    positive.isPositive = true;
+    positive.healthLevel = 0;
 }
